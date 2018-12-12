0af46cfb87ceaf0726fe8135e1f890a49dccfece
MatthewSchwartz6 <mschwartz66@gmail.com>
2018-12-11 22:00:07 -0500

    Deployed flask app with heroku through different repository.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup,find_packages
-setup(
-    name="MusicMentions",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=['en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.0.0/en_core_web_sm-2.0.0.tar.gz']
-)
